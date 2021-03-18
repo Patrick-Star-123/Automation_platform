f63d869d09dbf11b716eeac5926f08f19c52bb12
Patrick-Star-123 <80878189+Patrick-Star-123@users.noreply.github.com>
2021-03-18 14:58:17 +0800

    Add files via upload

---
+++ 批处理.bat
@@ -0,0 +1,8 @@
+::πÿ±’ªÿœ‘
+@echo off
+
+::÷¥–– airtest run √¸¡Ó
+
+start "b6254d85 " airtest run airtest_bat_run.air --device Android:///b6254d85  --log D:\work\Automation_platform\UI\Automated_test_platform\report\log\b6254d85 
+start "LMQ0220901000815" airtest run airtest_bat_run.air --device Android:///LMQ0220901000815 --log D:\work\Automation_platform\UI\Automated_test_platform\report\log\LMQ0220901000815
+start "638c1c6b" airtest run airtest_bat_run.air --device Android:///638c1c6b --log D:\work\Automation_platform\UI\Automated_test_platform\report\log\638c1c6b
